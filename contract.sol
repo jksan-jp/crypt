b208c8657337daccb88c74cb4405e7a80519486b
jksan <jksan.jp@gmail.com>
2022-06-11 23:39:29 +0900

    2-2

--- contract.sol
+++ contract.sol
@@ -14,6 +14,9 @@ contract ZombieFactory {
 
     Zombie[] public zombies;
 
+    mapping(uint256 => address) public zombieToOwner;
+    mapping(address => uint256) ownerZombieCount;
+
     function _createZombie(string memory _name, uint256 _dna) private {
         zombies.push(Zombie(_name, _dna));
         // ここでイベントを発生させるのだ
